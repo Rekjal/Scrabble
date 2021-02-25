59281f6f2c682e5865c4db824730f1074ceedf50
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-02-25 11:48:18 -0800

    Initial project setup
    Co-authored-by: Olha Hizhytska <olgainfotech@gmail.com>
    Co-authored-by: Salim Mayan <mailsalim@gmail.com>

---
+++ ModelTests/GameTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Game.Models;
+using System;
+
+namespace Game.Tests
+{
+  [TestClass]
+  public class GameTests {
+    
+  }
+
+}
